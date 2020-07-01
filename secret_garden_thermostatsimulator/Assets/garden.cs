f27e9b60871b0c8e1a579237cff3c00f9dd1d227
leonleonx <leon.michel97@yahoo.de>
2020-07-01 13:10:40 +0200

    Platzhalter für Haus, Plane für Boden

---
+++ secret_garden_thermostatsimulator/Assets/garden.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class garden : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject ground = GameObject.CreatePrimitive(PrimitiveType.Plane); 
+        ground.transform.localScale += new Vector3(2, 2, 2);
+
+        GameObject house = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        house.transform.localScale += new Vector3(2, 2, 2);
+        house.transform.position += new Vector3(12, 1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
