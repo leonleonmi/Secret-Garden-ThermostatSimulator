c07bc7da3e8336581401aeab826a913aab1eb408
leonleonx <leon.michel97@yahoo.de>
2020-08-09 17:01:10 +0200

    Charaktersteuerung, Kamera Anfang

---
+++ secret_garden_thermostatsimulator/Assets/CharacterController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A)) {
+			this.transform.rotation *= Quaternion.AngleAxis(-5.0f, Vector3.up);
+		}
+
+		if (Input.GetKey(KeyCode.D)){
+			this.transform.rotation *= Quaternion.AngleAxis(5.0f, Vector3.up);
+		}
+
+        if (Input.GetKey(KeyCode.W)){
+			this.transform.position += this.transform.localRotation * Vector3.forward/3; 
+		}
+
+        if (Input.GetKey(KeyCode.S)){
+			this.transform.position += (this.transform.localRotation * Vector3.forward/3)*-1; 
+		}
+		
+    }
+}
---
+++ secret_garden_thermostatsimulator/Assets/ThirdPersonCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+
+    public Transform target; 
+    public Vector3 offset;
+    public float smoothSpeed = 0.125f;
+    public float rotationSpeed = 20.0f;
+    
+
+    void LateUpdate() {
+
+
+        /*Quaternion camTurnAngle = 
+            Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, Vector3.up);
+            offset = camTurnAngle * offset;*/
+
+        Vector3 desiredPosition = target.position + offset;
+        transform.position = Vector3.Slerp(transform.position, desiredPosition, smoothSpeed);
+
+
+        
+        }
+    }
+    
+
