90a21b3d58585a9b092a7d1b688752704f04102a
leonleonx <leon.michel97@yahoo.de>
2020-09-07 22:43:19 +0200

    first person camera WIP 1

---
+++ secret_garden_thermostatsimulator/Assets/camController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camController : MonoBehaviour
+{
+
+    public float sensitivity = 100f;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(-Input.GetAxis("Mouse Y") * Time.deltaTime * sensitivity, 0, 0);
+        player.transform.Rotate(0, Input.GetAxis("Mouse X") * Time.deltaTime * sensitivity, 0);
+    }
+}
---
+++ secret_garden_thermostatsimulator/Assets/charController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 10f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+		float verticalMovement = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+		transform.Translate(horizontalMovement, 0, verticalMovement);
+    }
+}
