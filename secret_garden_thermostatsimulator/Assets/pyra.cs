8f4fa441927dbf7b5f32c9c77180b38a86c12d3e
Mihaly95 <67642248+Mihaly95@users.noreply.github.com>
2020-10-02 14:53:52 +0200

    Berg fertig + Wald angefangen

--- secret_garden_thermostatsimulator/Assets/pyra.cs
+++ secret_garden_thermostatsimulator/Assets/pyra.cs
@@ -29,6 +29,8 @@ public class pyra : MonoBehaviour
 	
 	void Start()
      {
+
+		 
 		vertices = new List<Vector3>();
 		normals = new List<Vector3>();
 		faces = new List<int>();
@@ -40,15 +42,30 @@ public class pyra : MonoBehaviour
 		//Haus erstellen
 		house = new GameObject("house");
 		house.AddComponent<MeshFilter>();
-		house.AddComponent<MeshRenderer>();   
+		house.AddComponent<MeshRenderer>();
+		house.AddComponent<MeshCollider>();   
 		meshHouse = house.GetComponent<MeshFilter>().mesh;
+		house.AddComponent<Rigidbody>().isKinematic = true;
+        
+
 		meshHouse.Clear();
 		house.transform.position = new Vector3((75), 20,221);
 
 		//Material des Hauses
 		Renderer rend = house.GetComponent<Renderer>();
 		rend.material = new Material(Shader.Find("Diffuse"));
+		rend.material.color = Color.white;
 
+
+		if (!meshHouse)
+        {
+            Debug.LogError("mesh");
+            return;
+        }
+        MeshCollider meshCollider = house.AddComponent<MeshCollider>();
+        meshCollider.sharedMesh = meshHouse;
+
+		
 		//Haus bauen
 		createTown();
 
@@ -100,11 +117,11 @@ public class pyra : MonoBehaviour
 	//In dieser Funktion wird ein Haus erstellt. Dazu werden die Vektorpunkte in einer createFace Funktion verbunden, um die jeweiligen Seiten des Hauses zu bilden
 	void createHouse(Vector3 position)
     {
-		float hoehe = Random.Range(1.0f, 10.0f);   //Der Y-Wert des Daches variiert von 3-9, damit die Haeuser unterschiedliche Hoehen haben
-		Vector3 a = new Vector3(8, 0, 8) + position;
-		Vector3 b = new Vector3(-8, 0, 8) + position;
-		Vector3 c = new Vector3(-8, 0, -8) + position;
-		Vector3 d = new Vector3(8, 0, -8) + position;
+		float hoehe = Random.Range(4.0f, 14.0f);   //Der Y-Wert des Daches variiert von 3-9, damit die Haeuser unterschiedliche Hoehen haben
+		Vector3 a = new Vector3(10, 0, 10) + position;
+		Vector3 b = new Vector3(-10, 0, 10) + position;
+		Vector3 c = new Vector3(-10, 0, -10) + position;
+		Vector3 d = new Vector3(10, 0, -10) + position;
 
 		Vector3 e = new Vector3(0, hoehe, 0) + position;
 		Vector3 f = new Vector3(0, hoehe, 0) + position;
@@ -129,11 +146,11 @@ public class pyra : MonoBehaviour
 
     void createHouse1(Vector3 position)
     {
-		float hoehe = Random.Range(1.0f, 2.0f);   //Der Y-Wert des Daches variiert von 3-9, damit die Haeuser unterschiedliche Hoehen haben
-		Vector3 a = new Vector3(2, 0, 2) + position;
-		Vector3 b = new Vector3(-2, 0, 2) + position;
-		Vector3 c = new Vector3(-2, 0, -2) + position;
-		Vector3 d = new Vector3(2, 0, -2) + position;
+		float hoehe = Random.Range(10.0f, 25.0f);   //Der Y-Wert des Daches variiert von 3-9, damit die Haeuser unterschiedliche Hoehen haben
+		Vector3 a = new Vector3(6, 0, 6) + position;
+		Vector3 b = new Vector3(-6, 0, 6) + position;
+		Vector3 c = new Vector3(-6, 0, -6) + position;
+		Vector3 d = new Vector3(6, 0, -6) + position;
 
 		Vector3 e = new Vector3(0, hoehe, 0) + position;
 		Vector3 f = new Vector3(0, hoehe, 0) + position;
@@ -164,16 +181,16 @@ public class pyra : MonoBehaviour
         {
 			for(int j = -3; j < 3; j = j + 2)
             {
-				Vector3 position = new Vector3((i *Random.Range(-9,8)), 0,(j*Random.Range(-8,8)));
+				Vector3 position = new Vector3((i *Random.Range(-7,7)), 0,(j*Random.Range(-5,8)));
 				createHouse(position);
             }
         }
 
-        for(int i = -5; i < 5; i++)
+        for(int i = -5; i < 5; i = i + 2)
         {
-			for(int j = -3; j < 3; j++)
+			for(int j = -3; j < 3; j = j + 2)
             {
-				Vector3 position = new Vector3((i *Random.Range(-9,8)), 0,(j*Random.Range(-8,8)));
+				Vector3 position = new Vector3((i *Random.Range(-8,7)), 0,(j*Random.Range(-5,8)));
 				createHouse1(position);
             }
         }
