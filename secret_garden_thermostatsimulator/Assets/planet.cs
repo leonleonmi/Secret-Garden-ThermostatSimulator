795719d8d8c3e9a97fa8c671c462bc894f2a4ea4
Mihaly95 <67642248+Mihaly95@users.noreply.github.com>
2020-10-05 23:46:22 +0200

    new Version
    Textur Planeten
    Lichter im Wald
    Geschwindigkeit Platten angepasst

--- secret_garden_thermostatsimulator/Assets/planet.cs
+++ secret_garden_thermostatsimulator/Assets/planet.cs
@@ -4,28 +4,41 @@ using UnityEngine;
 
 public class planet : MonoBehaviour
 {
-    public Texture texture;
-
+    
+    public Material brown;
+     
     
     // Start is called before the first frame update
     void Start()
     {
         createSpheres();
+        //planet.GetComponent<Renderer>().material = brown;
+        
+
     }
 void createSpheres()
     {
 		GameObject planet = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         planet.transform.localScale = new Vector3(10, 10, 10);
 		planet.name = "planet";
-		Renderer planetRenderer = planet.GetComponent<Renderer>();
-		planetRenderer.material.SetTexture("texture", texture);
+		planet.GetComponent<Renderer>().material = brown;
+        
+		planet.AddComponent<Rigidbody>().isKinematic = true;
 
+        
+
+       
+       
 		for (int i = 0; i < 70; i++)
         {
 			planet.transform.position = new Vector3(Random.Range(-500.0f, 70.0f),Random.Range(15.0f, 240.0f), Random.Range(-500.0f, -30.0f));
 			Instantiate(planet);
         }
 	}
+
+    
+
+   
     // Update is called once per frame
     void Update()
     {
