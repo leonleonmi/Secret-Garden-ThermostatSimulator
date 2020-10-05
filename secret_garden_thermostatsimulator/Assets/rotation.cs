c182ecfee215f137de97bff6b7fb890a4b7373d9
leonleonx <leon.michel97@yahoo.de>
2020-10-05 15:35:45 +0200

    Collectibles angefangen

---
+++ secret_garden_thermostatsimulator/Assets/rotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+
+    public GameObject pasta;
+    public GameObject sauce;
+    public GameObject herbs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pasta.transform.Rotate(0, 1.5f, 0);
+        sauce.transform.Rotate(0, 1.5f, 0);
+        herbs.transform.Rotate(0, 1.5f, 0);
+    }
+}
