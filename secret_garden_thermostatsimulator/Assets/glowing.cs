a1c5d51fe4c33ad5e713d58347a1aef59b1d3d00
Mihaly95 <67642248+Mihaly95@users.noreply.github.com>
2020-10-07 12:02:21 +0200

    Lichter + Steuerung überarbeitet

--- secret_garden_thermostatsimulator/Assets/glowing.cs
+++ secret_garden_thermostatsimulator/Assets/glowing.cs
@@ -13,12 +13,14 @@ public class glowing : MonoBehaviour
     void createGlowing()
     {
 		GameObject glow = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        glow.transform.localScale = new Vector3(1, 1, 1);
+        Light lightComp = glow.AddComponent<Light>();
+        glow.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
 		glow.name = "glowing";
+        lightComp.color = Color.green;
 		//glow.GetComponent<Renderer>().material = brown;
         glow.AddComponent<Rigidbody>().isKinematic = true;
 
-        for (int i = 0; i < 200; i++)
+        for (int i = 0; i < 400; i++)
         {
 			glow.transform.position = new Vector3(Random.Range(200f, 140f),Random.Range(20, 30f), Random.Range(90f, 150f));
 			Instantiate(glow);
---
+++ secret_garden_thermostatsimulator/Assets/spaceController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spaceController : MonoBehaviour
+{
+    public float forward = 35f;
+    public float forwardSpeedUp = 20f; 
+    public float rotateSpeed = 93f;
+    public float rollingSpeed = 91f;
+    public float rollingSpeedUp = 4f;
+    public float getforward; 
+    public float roll;
+
+    public Vector2 middleOfScreen; 
+    public Vector2 wayMouse;
+    public Vector2 showInput;
+
+    public bool contact = false;
+    public GameObject ShipCam;
+    public GameObject PlayerOne;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        middleOfScreen.x = Screen.width * .5f;
+        middleOfScreen.y = Screen.height * .5f;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    void Update()
+    {
+        if (ShipCam.active == true) {
+        PlayerOne.SetActive(false);
+        showInput.x = Input.mousePosition.x;
+        showInput.y = Input.mousePosition.y;
+
+        wayMouse.x = (showInput.x - middleOfScreen.x) / middleOfScreen.x;
+        wayMouse.y = (showInput.y - middleOfScreen.y) / middleOfScreen.y;
+        wayMouse = Vector2.ClampMagnitude(wayMouse, 10f);
+
+        getforward = Mathf.Lerp(getforward, Input.GetAxisRaw("Vertical") * forward, forwardSpeedUp * Time.deltaTime);
+        roll = Mathf.Lerp(roll, Input.GetAxisRaw("Roll"), rollingSpeedUp * Time.deltaTime);
+        transform.Rotate(-wayMouse.y * rotateSpeed * Time.deltaTime, wayMouse.x * rotateSpeed * Time.deltaTime, roll * rollingSpeed * Time.deltaTime, Space.Self); 
+        transform.position += transform.forward * getforward * Time.deltaTime;
+        
+    }
+    }
+
+}
